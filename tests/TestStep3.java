3ccf4f0b47f03366760bc6a1e9b4d19109445146
Your Name <you@example.com>
2024-09-28 15:54:37 +0200

    red : hand(A,8) => 19 + !blackjack + !busted

--- tests/TestStep3.java
+++ tests/TestStep3.java
@@ -34,17 +34,17 @@ public class TestStep3 {
 		assertFalse(hand.isBusted());
 	}
 
-//	@Test
-//	public void should_not_have_blackjack_when_cards_are_ace_and_eight() {
-//		ICard firstCard = new Card("A");
-//		ICard secondCard = new Card("8");
-//		IHand hand = new Hand(firstCard, secondCard);
-//
-//		assertTrue(19 == hand.getPoints());
-//		assertFalse(hand.isBlackJack());
-//		assertFalse(hand.isBusted());
-//	}
-//
+	@Test
+	public void should_not_have_blackjack_when_cards_are_ace_and_eight() {
+		ICard firstCard = new Card("A");
+		ICard secondCard = new Card("8");
+		IHand hand = new Hand(firstCard, secondCard);
+
+		assertTrue(19 == hand.getPoints());
+		assertFalse(hand.isBlackJack());
+		assertFalse(hand.isBusted());
+	}
+
 //	@Test
 //	public void should_be_busted_when_score_goes_above_21() {
 //		ICard firstCard = new Card("10");
