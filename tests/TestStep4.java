39b196dafda7b584f19730655528483a7cb7fc39
Your Name <you@example.com>
2024-09-28 16:19:29 +0200

    green : hand(10,4,7,A) => 22 + !blackjack + busted

--- tests/TestStep4.java
+++ tests/TestStep4.java
@@ -22,23 +22,23 @@ public class TestStep4 {
 		assertFalse(hand.isBlackJack());
 		assertFalse(hand.isBusted());
 	}
-//
-//	@Test
-//	public void should_have_twenty_two_and_busted_when_cards_are_ten_and_four_and_seven_and_ace() {
-//		ICard firstCard = new Card("10");
-//		ICard secondCard = new Card("4");
-//		ICard thirdCard = new Card("7");
-//		ICard fourthCard = new Card("A");
-//		IHand hand = new Hand(firstCard, secondCard);
-//
-//		hand.addCard(thirdCard);
-//		hand.addCard(fourthCard);
-//
-//		assertTrue(22 == hand.getPoints());
-//		assertFalse(hand.isBlackJack());
-//		assertTrue(hand.isBusted());
-//	}
-//
+
+	@Test
+	public void should_have_twenty_two_and_busted_when_cards_are_ten_and_four_and_seven_and_ace() {
+		ICard firstCard = new Card("10");
+		ICard secondCard = new Card("4");
+		ICard thirdCard = new Card("7");
+		ICard fourthCard = new Card("A");
+		IHand hand = new Hand(firstCard, secondCard);
+
+		hand.addCard(thirdCard);
+		hand.addCard(fourthCard);
+
+		assertTrue(22 == hand.getPoints());
+		assertFalse(hand.isBlackJack());
+		assertTrue(hand.isBusted());
+	}
+
 //	@Test
 //	public void should_have_thirteen_when_cards_are_ace_and_ace_and_ace() {
 //		ICard firstCard = new Card("A");
