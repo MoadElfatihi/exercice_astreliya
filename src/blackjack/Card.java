fe4b9acaaf6f5eca8897c6153f352e44f5aaa6ec
Your Name <you@example.com>
2024-10-22 11:26:49 +0200

    Step 1: Add Card value implementation and tests

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -2,29 +2,38 @@ package blackjack;
 
 public class Card implements ICard {
 
+	// Value must have a value
 	private final String value;
-
 	public Card(String value) {
 		this.value = value;
 	}
 	
 	public int getPoints() {
-		if (isKingOrJack()) {
-			return 10;
+		int points = 0;
+		switch (this.value) {
+			case "J":
+			case "Q":
+			case "K":
+				points = 10;
+				break;
+			case "1":
+			case "A":
+				points = 11;
+				break;
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+			case "10":
+				points = Integer.parseInt(this.value);
+				break;
+			default:
+				throw new IllegalArgumentException("Invalid card");
 		}
-		if (isAce()) {
-			return 11;
-		}
-
-		return Integer.parseInt(this.value);
+		return points;
 	}
-
-	private boolean isKingOrJack() {
-		return this.value.equals("K") || this.value.equals("J");
-	}
-
-	private boolean isAce() {
-		return this.value.equals("A") || this.value.equals("1");
-	}
-
 }
